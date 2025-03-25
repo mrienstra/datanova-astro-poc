25a42343dba28526969003962c304b72a75b2bbf
Michael Rienstra <mikey@unfk.com>
2025-03-25 14:45:14 -0700

    drop DB & Svelte (feedback component)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-import db from "@astrojs/db";
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  // The `site` property specifies the base URL for your site.
-  // Be sure to update this to your own domain (e.g., "https://yourdomain.com") before deploying.
+  // The `site` property specifies the base URL for your site
   site: process.env.SITE_CANONICAL_URL,
   prefetch: true,
   trailingSlash: "never",
@@ -21,8 +18,6 @@ export default defineConfig({
     react(),
     markdoc(),
     ...(process.env.SKIP_KEYSTATIC ? [] : [keystatic()]),
-    db(),
-    svelte(),
   ],
   vite: {
     plugins: [tailwindcss()],
